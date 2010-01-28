7142e7200d33cd99e40909470a832345c0ad094e
Vladimir Sizikov <vsizikov@gmail.com>
2010-01-28 19:39:15 +0100

    Unexclude test, since Nick has already fixed the launcher.
    It was printing to CON! :)

--- spec/launcher_spec.rb
+++ spec/launcher_spec.rb
@@ -9,12 +9,7 @@ describe "JRuby native launcher" do
     jruby_launcher_args("-J-Dsome.option -v --help")[-3..-1].should == ["org/jruby/Main", "-v", "--help"]
   end
 
-  # TODO: Fails on Windows for no apparent reason.
-  # Most probably we do some weird stuff in Lanucher C++ code,
-  # so that 2>&1 isn't redirecting!
-  unless JRubyLauncherHelper::WINDOWS
-    it "should print help message" do
-      jruby_launcher("-Xhelp 2>&1").should =~ /JRuby Launcher usage/
-    end
+  it "should print help message" do
+    jruby_launcher("-Xhelp 2>&1").should =~ /JRuby Launcher usage/
   end
 end
