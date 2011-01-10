dc6bad98919fecab6c99759537a47f9c3761cba6
Nick Sieger <nick@nicksieger.com>
2011-01-10 11:26:22 -0600

    Bump to 1.0.4

--- version.h
+++ version.h
@@ -1,11 +1,11 @@
 /*
- * Copyright 2009-2010 JRuby Team (www.jruby.org).
+ * Copyright 2009-2011 JRuby Team (www.jruby.org).
  */
 
 
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define JRUBY_LAUNCHER_VERSION "1.0.3"
+#define JRUBY_LAUNCHER_VERSION "1.0.4"
 
 #endif // ! _VERSION_H_
