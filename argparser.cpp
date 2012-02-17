540d7aaecf82ac2471ca6256c59266cdddfc3dfe
Charles Oliver Nutter <headius@headius.com>
2012-02-17 03:35:03 -0600

    Remove JRuby-specific properties for reporting heap and stack size.
    As of JRuby 1.6.7, we will use RuntimeMXBean to report any -Xmx
    and -Xss values passed to Java, rather than relying on a JRuby-
    specific property. This change removes setting this property from
    jruby-launcher.
    
    NOTE: This change should NOT be incorporated into a jruby-launcher
    release until long after 1.6.7 has been out, since previous
    releases still rely on the property being set to report memory
    and stack errors.

--- argparser.cpp
+++ argparser.cpp
@@ -465,8 +465,6 @@ void ArgParser::setupMaxHeapAndStack(list<string> userOptions) {
     if (!maxStack) {
         javaOptions.push_back("-Xss" + stackSize);
     }
-    javaOptions.push_back("-Djruby.memory.max=" + heapSize);
-    javaOptions.push_back("-Djruby.stack.max=" + stackSize);
 }
 
 void ArgParser::constructBootClassPath() {
