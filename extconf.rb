c3d3c85a39b6af119f64f059c8dd24635e311724
Charles Oliver Nutter <headius@headius.com>
2018-04-18 10:08:49 -0500

    Don't use antiquated patterns.

--- extconf.rb
+++ extconf.rb
@@ -1,7 +1,5 @@
-require 'rbconfig'
-
 mf = File.read('Makefile')
-mf = mf.gsub(/^BINDIR\s*=.*$/, "BINDIR = #{Config::CONFIG['bindir']}")
-mf = mf.gsub(/^PREFIX\s*=.*$/, "PREFIX = #{File.dirname(Config::CONFIG['libdir'])}")
+mf = mf.gsub(/^BINDIR\s*=.*$/, "BINDIR = #{RbConfig::CONFIG['bindir']}")
+mf = mf.gsub(/^PREFIX\s*=.*$/, "PREFIX = #{File.dirname(RbConfig::CONFIG['libdir'])}")
 puts mf
 File.open('Makefile', 'wb') {|f| f << mf}
