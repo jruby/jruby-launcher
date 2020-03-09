341ef15d9c8eccb99f2fe2d4afb7ab5f71ce1122
Charles Oliver Nutter <headius@headius.com>
2020-03-09 18:51:30 -0500

    Bad regex matched 9.2.9.0

--- extconf.rb
+++ extconf.rb
@@ -6,7 +6,7 @@ mf = mf.gsub(/^JRUBY_VERSION\s*=.*$/, "JRUBY_VERSION = #{JRUBY_VERSION}")
 # Launcher will use .module_opts file if present, otherwise hardcoded add-opens for this module.
 # Pre-9.2.1: ALL-UNNAMED because no name was exported
 # 9.2.1 and higher: org.jruby.dist
-if JRUBY_VERSION !~ /(^1)|(9\.[01])|(9.2.0.0)/
+if JRUBY_VERSION !~ /(^1)|(^9\.[01])|(^9\.2\.0\.0)/
   mf = mf.gsub(/^JRUBY_MODULE\s*=.*$/, "JRUBY_MODULE = 1")
 end
 puts mf
