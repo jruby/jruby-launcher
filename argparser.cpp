e2cbfab27158d6705db133e8dd96cf025d5e35e7
Thomas E. Enebo <tom.enebo@gmail.com>
2020-03-20 12:11:18 -0500

    dead code

--- argparser.cpp
+++ argparser.cpp
@@ -250,7 +250,6 @@ bool ArgParser::parseArgs(int argc, char *argv[]) {
 
     // Force OpenJDK-based JVMs to use /dev/urandom for random number generation
     // See https://github.com/jruby/jruby/issues/4685 among others.
-    struct stat buffer;
     if (access("/dev/urandom", R_OK) == 0) {
         // OpenJDK tries really hard to prevent you from using urandom.
         // See https://bugs.openjdk.java.net/browse/JDK-6202721
